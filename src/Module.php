fe708e821d8774f82dbabfe681f267c86d57f9f9
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-11-15 05:56:29 +0000

    Remove 'view_manager' settings which could override application

--- src/Module.php
+++ src/Module.php
@@ -32,18 +32,8 @@ class Module
                 ],
             ],
             'view_manager' => [
-                'display_not_found_reason' => true,
-                'doctype'                  => 'HTML5',
-                'not_found_template'       => 'error/404',
-                'exception_template'       => 'error/index',
-                'template_map' => [
-                    'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-                    'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-                    'error/404'               => __DIR__ . '/../view/error/404.phtml',
-                    'error/index'             => __DIR__ . '/../view/error/index.phtml',
-                ],
                 'template_path_stack' => [
-                    'application' => __DIR__ . '/../view',
+                    'monthly-basis/question-https' => __DIR__ . '/../view',
                 ],
             ],
         ];
